817b344d6e60bba3ff3eb9f5fd62c0981705bae8
我是个宝宝 <13020106399@163.com>
2021-01-12 17:37:17 +0800

    project init

--- src/start.js
+++ src/start.js
@@ -4,18 +4,20 @@
  */
 
 'use strict';
-import {invoke} from './navigation/invoke';
+import {
+  invoke
+} from './navigation/invoke';
 
 let started = false;
 
 export function start() {
-    if (started) {
-        return;
-    }
-    started = true;
-    return invoke();
+  if (started) {
+    return;
+  }
+  started = true;
+  return invoke();
 }
 
 export function isStarted() {
-    return started;
+  return started;
 }
